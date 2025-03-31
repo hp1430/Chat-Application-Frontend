073d3f1b26e8071bb3c16db675012dea71e40537
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-03-31 09:41:56 +0530

    Added signin functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import './App.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Route, Routes } from 'react-router-dom';
 
 import { Toaster } from '@/components/ui/toaster';
-import { Auth } from '@/pages/Auth/Auth';
-import { NotFound } from '@/pages/NotFound/NotFound';
+import { AppRoutes } from '@/Routes';
 
-import { EmailVerification } from './components/molecules/EmailVerification/EmailVerification';
-import { EmailVerificationRequest } from './components/molecules/EmailVerification/EmailVerificationRequest';
-import { ResetPasswordContainer } from './components/molecules/ResetPassword/ResetPasswordContainer';
-import { SigninContainer } from './components/organisms/Auth/SigninContainer';
-import { SignupContainer } from './components/organisms/Auth/SignupContainer';
-import { ForgotPasswordContainer } from './components/organisms/ForgotPassword/ForgotPasswordContainer';
+import { AppContextProvider } from './context/AppContextProvider';
 
 function App() {
 
@@ -20,17 +13,9 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/auth/signup" element={<Auth><SignupContainer /></Auth>} />
-        <Route path="/auth/signin" element={<Auth><SigninContainer /></Auth>} />
-        <Route path='/home' element={<Auth><h1>Home</h1></Auth>} />
-        <Route path='/forgotpassword/request' element={<Auth><ForgotPasswordContainer /></Auth>} />
-        <Route path='/passwordreset' element={<Auth><ResetPasswordContainer /></Auth>} />
-        <Route path='/users/verify/:token' element={<Auth><EmailVerification /></Auth>} />
-        <Route path='/users/verify' element={<Auth><EmailVerificationRequest /></Auth>} />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AppContextProvider>
+        <AppRoutes />
+      </AppContextProvider>
       <Toaster />
     </QueryClientProvider>
     
---
+++ src/Routes.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { EmailVerification } from '@/components/molecules/EmailVerification/EmailVerification';
+import { EmailVerificationRequest } from '@/components/molecules/EmailVerification/EmailVerificationRequest';
+import { ResetPasswordContainer } from '@/components/molecules/ResetPassword/ResetPasswordContainer';
+import { SigninContainer } from '@/components/organisms/Auth/SigninContainer';
+import { SignupContainer } from '@/components/organisms/Auth/SignupContainer';
+import { ForgotPasswordContainer } from '@/components/organisms/ForgotPassword/ForgotPasswordContainer';
+import { Auth } from '@/pages/Auth/Auth';
+import { NotFound } from '@/pages/NotFound/NotFound';
+
+import { ProtectedRoute } from './components/molecules/ProtectedRoute/ProtectedRoute';
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+          <Route path="/auth/signup" element={<Auth><SignupContainer /></Auth>} />
+          <Route path="/auth/signin" element={<Auth><SigninContainer /></Auth>} />
+          <Route path='/home' element={<ProtectedRoute><Auth><h1>Home</h1></Auth></ProtectedRoute>} />
+          <Route path='/forgotpassword/request' element={<Auth><ForgotPasswordContainer /></Auth>} />
+          <Route path='/passwordreset' element={<Auth><ResetPasswordContainer /></Auth>} />
+          <Route path='/users/verify/:token' element={<Auth><EmailVerification /></Auth>} />
+          <Route path='/users/verify' element={<Auth><EmailVerificationRequest /></Auth>} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
+};
