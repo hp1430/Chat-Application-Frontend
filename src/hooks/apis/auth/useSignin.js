84051f3a762c9a439661f76b87368b879ef246f9
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-03-31 09:41:56 +0530

    Added signin functionality

--- src/hooks/apis/auth/useSignin.js
+++ src/hooks/apis/auth/useSignin.js
@@ -1,14 +1,26 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { signInRequest } from '@/apis/auth';
+import { useAuth } from '@/hooks/context/useAuth';
 import { useToast } from '@/hooks/use-toast';
 
 export const useSignin = () => {
     const { toast } = useToast();
+    const { setAuth } = useAuth();
     const { isSuccess, isPending, error, mutateAsync: signinMutation } = useMutation({
         mutationFn: signInRequest,
-        onSuccess: (data) => {
-            console.log('Succesfully signed in ', data);
+        onSuccess: (response) => {
+            console.log('Succesfully signed in ', response);
+
+            const userObject = JSON.stringify(response.data);
+            localStorage.setItem('user', userObject);
+            localStorage.setItem('token', response.data.token);
+            setAuth({
+                token: response.data.token,
+                user: userObject,
+                isLoading: false
+            });
+            
             toast({
                 title: 'Successfully signed in',
                 message: 'You will be redirected to homepage in a few seconds',
