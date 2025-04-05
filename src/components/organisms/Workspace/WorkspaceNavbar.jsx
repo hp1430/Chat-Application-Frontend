438c0fcb0770cce5558305a0c3b9feb65387c79f
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-04-05 09:12:20 +0530

    Added more workspace ui

---
+++ src/components/organisms/Workspace/WorkspaceNavbar.jsx
@@ -0,0 +1,44 @@
+import { InfoIcon, LucideLoader2, SearchIcon } from 'lucide-react';
+import { useParams } from 'react-router-dom';
+
+import { Button } from '@/components/ui/button';
+import { useGetWorkspaceById } from '@/hooks/workspaces/useGetWorkspaceById';
+
+export const WorkspaceNavbar = () => {
+
+    const { workspaceId } = useParams();
+    const { isFetching, workspace } = useGetWorkspaceById(workspaceId);
+
+    if(isFetching) {
+        return <LucideLoader2 className='animate-spin ml-2' />;
+    }
+
+    return (
+        <nav
+            className='flex items-center justify-center h-10 p-1.5 bg-slack-DARK'
+        >
+            <div className='flex-1' />
+            <div>
+                <Button
+                    size='sm'
+                    className='bg-accent/25 hover:bg-accent/15 w-full justify-start h-7 px-2'
+                >
+                    <SearchIcon className='text-white size-5 mr-2' />
+                    <span className='text-white text-xs'>
+                        Search {workspace?.name || 'Workspace'}
+                    </span>
+                </Button>
+            </div>
+            <div
+                className='ml-auto flex-1 flex items-center justify-end'
+            >
+                <Button
+                    variant='transparent'
+                    size='sm'
+                >
+                    <InfoIcon className='text-white size-5' />
+                </Button>
+            </div>
+        </nav>
+    );
+};
--- src/components/organisms/Workspace/WorkspaceSidebar.jsx
+++ src/components/organisms/Workspace/WorkspaceSidebar.jsx
@@ -3,11 +3,14 @@ import { BellIcon, HomeIcon, MessageSquareIcon, MoreHorizontalIcon } from 'lucid
 import { UserButton } from '@/components/atoms/UserButton/UserButton';
 import { SidebarButton } from '@/components/molecules/SidebarButton/SidebarButton';
 
+import { WorkspaceSwitcher } from './WorkspaceSwitcher';
+
 export const WorkspaceSidebar = () => {
     return (
         <aside
             className="w-[70px] h-full bg-slack-DARK flex flex-col gap-y-4 items-center pt-[10px] pb-[5px]"
         >
+            <WorkspaceSwitcher />
             <SidebarButton 
                 Icon={HomeIcon}
                 label='Home'
