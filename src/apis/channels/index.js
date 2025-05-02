a956900da9596d41951275ad69b43133c5073cc8
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-05-02 09:38:35 +0530

    Added quill setup

---
+++ src/apis/channels/index.js
@@ -0,0 +1,16 @@
+import axios from '@/config/axiosConfig';
+
+export const getChannelById = async({ channelId, token }) => {
+    try {
+        const response = await axios.get(`/channels/${channelId}`, {
+            headers: {
+                'x-access-token': token
+            }
+        });
+        return response?.data?.data;
+    }
+    catch(error) {
+        console.error('Error fetching channel by ID:', error);
+        throw error;
+    }
+};
