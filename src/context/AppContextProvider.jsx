7bbcb38800f6d6049875f7336b37961a5ce1a652
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-06-03 10:15:47 +0530

    Bug Fix

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -3,7 +3,7 @@ import combineContext from '@/utils/combineContext';
 import { AuthContextProvider } from './AuthContext';
 import { ChannelMessagesProvider } from './ChannelMessages';
 import { CreateChannelContextProvider } from './CreateChannelContext';
-import { CreateWorkspaceContextProvider } from './CreateWorkspaceCOntext';
+import { CreateWorkspaceContextProvider } from './CreateWorkspaceContext';
 import { SocketContextProvider } from './SocketContext';
 import { WorkspaceContextProvider } from './WorkspaceContext';
 import { WorkspacePreferencesModalContextProvider } from './WorkspacePreferencesModalContext';
