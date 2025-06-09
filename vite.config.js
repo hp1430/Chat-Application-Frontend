2a7ed4910bd115e843fc8f702e758e3e2f6acc42
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-06-09 19:07:43 +0530

    Update env handling in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    define: {
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+      'process.env.VITE_BACKEND_API_URL': JSON.stringify(env.VITE_BACKEND_API_URL),
+      'process.env.VITE_BACKEND_SOCKET_URL': JSON.stringify(env.VITE_BACKEND_SOCKET_URL)
+    }
+  };
 });
