357a1f5f433e48b4a32ea6a51b028f489205076e
Himanshu Parashar <parasharhimanshu41@gmail.com>
2025-07-06 12:42:59 +0530

    added vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.');
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    define: {
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
-      'process.env.VITE_BACKEND_API_URL': JSON.stringify(env.VITE_BACKEND_API_URL),
-      'process.env.VITE_BACKEND_SOCKET_URL': JSON.stringify(env.VITE_BACKEND_SOCKET_URL)
-    }
-  };
+  },
 });
